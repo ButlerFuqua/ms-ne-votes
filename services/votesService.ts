ccca06cdecfa22b7c2861ed6cc605d9a98ad4999
butlerfuqua <butlerfuqua@gmail.com>
2024-09-10 21:11:43 -1000

    Add searchBills method

--- services/votesService.ts
+++ services/votesService.ts
@@ -1,3 +1,4 @@
+import { BillsDbService } from "../persistance/billsDbService.ts";
 import { BillsService } from "./billsService.ts";
 
 
@@ -10,13 +11,7 @@ export class VotesService{
     }
 
     searchAll(){
-        return [
-            {
-                id: 3,
-                title: "This is the title of the vote",
-                description: "This is the description to the vote. It is a good vote. Please don't read the fine print. Just vote yest. Do it."
-            }
-        ]
+        return BillsDbService.searchBills();
     }
 
     async createAndStoreVotes(){
